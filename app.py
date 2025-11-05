8f26036cdd37472f4f9a0a8d426b304af031e36e
Max Ostapiak <maksym.ostapiak.23@pnu.edu.ua>
2025-11-05 20:00:40 +0200

    lab2 resume and contacts

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'Hello, world!'
+def resume():
+    return render_template(
+        'resume.html',
+        page_title='Резюме',
+        header_title='Моє резюме'
+    )
+
+@app.route('/contacts')
+def contacts():
+    return render_template(
+        'contacts.html',
+        page_title='Контакти',
+        header_title='Зв’язок зі мною'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
