08db6b39d2947bd705678f8ec882f9250b78edfc
Max Ostapiak <maksym.ostapiak.23@pnu.edu.ua>
2025-11-06 20:01:53 +0200

    Added profile, themes, login and flash warning

--- app/users/views.py
+++ app/users/views.py
@@ -1,16 +1,68 @@
-from flask import Blueprint, request, redirect, url_for, render_template
+from flask import Blueprint, request, redirect, url_for, render_template, flash, session, make_response
 
 users_bp = Blueprint('users', __name__, url_prefix='/users', template_folder='templates')
 
-@users_bp.route("/hi/<string:name>")
-def greetings(name):
-    name = name.upper()
-    age = request.args.get("age", None, int)
+VALID_USERNAME = "admin"
+VALID_PASSWORD = "12345"
 
-    return render_template("users/hi.html", name=name, age=age)
+@users_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-@users_bp.route("/admin")
-def admin():
-    to_url = url_for("users.greetings", name="administrator", age=45, _external=True)
-    print(to_url)
-    return redirect(to_url)
+        if username == VALID_USERNAME and password == VALID_PASSWORD:
+            session["user"] = username  
+            flash("Ви успішно увійшли!", "success")
+            return redirect(url_for("users.profile"))
+        else:
+            flash("Невірне ім’я користувача або пароль!", "danger")
+            return redirect(url_for("users.login"))
+
+    return render_template("users/login.html")
+
+@users_bp.route("/profile", methods=["GET", "POST"])
+def profile():
+    user = session.get("user")
+    resp = make_response()
+    if not user:
+        flash("Будь ласка, увійдіть у систему!", "warning")
+        return redirect(url_for("users.login"))
+
+    if request.method == 'POST' and 'add_cookie' in request.form:
+        key = request.form['key']
+        value = request.form['value']
+        days = int(request.form['days'])
+        resp.set_cookie(key, value, max_age=days*24*60*60)
+        flash(f"Cookie '{key}' додано!", "success")
+
+    if request.method == 'POST' and 'delete_cookie' in request.form:
+        key = request.form['del_key']
+        resp.delete_cookie(key)
+        flash(f"Cookie '{key}' видалено!", "warning")
+
+    if request.method == 'POST' and 'delete_all' in request.form:
+        for key in request.cookies:
+            resp.delete_cookie(key)
+        flash("Всі cookies видалено!", "danger")
+
+    resp.response = render_template("users/profile.html",username=user)
+    return resp
+
+
+@users_bp.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash("Ви вийшли із системи!", "info")
+    return redirect(url_for("users.login"))
+
+
+@users_bp.route("/theme", methods=["GET", "POST"])
+def theme():
+    if request.method == "POST":
+        theme = request.form.get("theme", "light")
+        resp = make_response(redirect(url_for("users.profile")))
+        resp.set_cookie("theme", theme, max_age=60*60*24*30)
+        return resp
+
+    return render_template("users/theme.html")
--- app/views.py
+++ app/views.py
@@ -2,5 +2,17 @@ from flask import request, redirect, url_for, render_template, abort
 from . import app
 
 @app.route('/')
-def main():
-    return render_template("base.html")
+def resume():
+    return render_template(
+        'resume.html',
+        page_title='Резюме',
+        header_title='Моє резюме'
+    )
+
+@app.route('/contacts')
+def contacts():
+    return render_template(
+        'contacts.html',
+        page_title='Контакти',
+        header_title='Зв’язок зі мною'
+    )
